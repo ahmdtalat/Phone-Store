4921d9947852a84e69f2a68c0228de03c9abcc7d
ahmdtalaat <ahmd.talat95@gmail.com>
2019-09-14 11:34:58 +0200

    Change navbar using styled components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button';
 import logo from '../logo.svg';
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm bg-dark navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */}
+            Creative Commons (Attribution 3.0 Unported);
+            https://www.iconfinder.com/Makoto_msk */}
+
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -19,13 +22,22 @@ https://www.iconfinder.com/Makoto_msk */}
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" /> my cart
-          </button>
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" /> my cart
+            </span>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     </div>
   );
 };
-
+const NavWrapper = styled.nav`
+  background: var(--mainDark);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
 export default Navbar;
