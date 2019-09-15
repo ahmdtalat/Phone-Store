936f7ae64718da307342669fbf03d22a4e6a170a
ahmdtalaat <ahmd.talat95@gmail.com>
2019-09-15 15:13:33 +0200

    page not found

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -6,7 +6,7 @@ const CartTotals = ({ cartTotal, cartSubTotal, cartTax, clearCart }) => {
     <Fragment>
       <div className="container">
         <div className="row">
-          <div className="col-sm-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
             <Link to="/">
               <button
                 className="btn btn-outline-danger text-uppercase"
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const NotFound = () => {
+const NotFound = (props) => {
   return (
-    <div>
-      <h3>Hello from notFound</h3>
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto text-title text-center text-uppercase pt-5">
+          <h1 className="display-3 my-auto">error</h1>
+          <h1>404</h1>
+          <h1>page not found</h1>
+          <span className="text-danger">
+            requested url "{props.location.pathname}" not found
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
